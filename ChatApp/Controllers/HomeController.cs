fe85dc577053ef7dc2d670fe56990d2cc6c5b7e3
Jack <Jacksonpaul.thiraviaraj@syncfusion.com>
2025-05-30 18:55:44 +0530

    SAST fix

--- ChatApp/Controllers/HomeController.cs
+++ ChatApp/Controllers/HomeController.cs
@@ -20,7 +20,6 @@ namespace ChatApp.Controllers
         public IActionResult ChatPage(string username)
         {
             TempData["username"] = username;
-            Redirect(username);
             return View();
         }
         public IActionResult Index()
